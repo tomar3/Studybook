4fdc237f5f89e5222b4c41e12cca00c79c61371f
tomar3 <talab.omar@gmail.com>
2017-11-06 11:56:14 -0600

    fix: Remove overdraw from material drawer theme in dashboard activity

--- android/Studybook/app/src/main/java/com/codertal/studybook/features/dashboard/DashboardActivity.java
+++ android/Studybook/app/src/main/java/com/codertal/studybook/features/dashboard/DashboardActivity.java
@@ -34,6 +34,7 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        getWindow().setBackgroundDrawable(null);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
